bc565848221c0293d086ec8078b0d73bb1877e01
Kyle Messerhuber <kylemesserhuber@gmail.com>
2019-01-12 00:38:37 -0500

    PLATES!

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -7,18 +7,28 @@ public class Plate : Interactable
 
     public FoodItem itemOnPlate = null;
 
+    GameObject itemOnPlateInteractable;
+
     public override void Interact()
     {
         // If the player's inventory has an item, remove it and spawn it on the plate
         if (GameManager.instance.playerInventory.currentItem != null)
         {
             itemOnPlate = GameManager.instance.playerInventory.currentItem;
-            Instantiate(itemOnPlate.foodInteractable.gameObject, transform.position, Quaternion.identity);
+            itemOnPlateInteractable = 
+                Instantiate(itemOnPlate.foodInteractable.gameObject, transform.position, Quaternion.identity);
 
 
             GameManager.instance.playerInventory.currentItem = null;
-
+            Destroy(GameManager.instance.playerInventory.itemGameObject);
+        }
+        // If the player instead wants to pick up the item ON the plate
+        else if (GameManager.instance.playerInventory.currentItem == null && itemOnPlate != null)
+        {
+            GameManager.instance.playerInventory.currentItem = itemOnPlate;
+            GameManager.instance.playerInventory.SpawnItem();
+            itemOnPlate = null;
+            Destroy(itemOnPlateInteractable);
         }
     }
-
 }
